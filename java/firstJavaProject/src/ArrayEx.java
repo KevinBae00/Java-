d939c4a8526912b89fb8479f24951aa02c599206
Q-Ri <kimchaerin2@gmail.com>
2023-05-19 18:04:13 +0900

    05-19

--- java/firstJavaProject/src/ArrayEx.java
+++ java/firstJavaProject/src/ArrayEx.java
@@ -145,6 +145,35 @@ public class ArrayEx {
 //        }
 //        System.out.println(distinct);
 
+        char[][] star = {
+                {'*', '*', ' ', ' ', ' '},
+                {'*', '*', ' ', ' ', ' '},
+                {'*', '*', '*', '*', '*'},
+                {'*', '*', '*', '*', '*'},
+        };
 
+        for (int i = 0; i < star.length; i++) {
+            for (int j = 0; j < star[i].length; j++) {
+                System.out.print(star[i][j]);
+            }
+            System.out.println();
+        }
+
+        char[][] star90 = new char[star[0].length][star.length];
+
+        for (int i = 0; i < star.length; i++) {
+            for (int j = 0; j < star[i].length; j++) {
+                int y = star.length - 1 - i;
+                star90[j][y] = star[i][j];
+            }
+        }
+
+        for (int i = 0; i < star90.length; i++) {
+            for (int j = 0; j < star90[i].length; j++) {
+                System.out.print(star90[i][j]);
+            }
+            System.out.println();
+        }
+       
     }
 }
