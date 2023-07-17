824a9cd35708181d8bc2b3d0e0693a9cdfcf956d
dldlfdl2 <s21plus.choi@gmail.com>
2023-07-17 14:55:57 +0900

    07-17

--- jsp/Spring/boardWeb/src/main/java/com/example/view/user/LoginController.java
+++ jsp/Spring/boardWeb/src/main/java/com/example/view/user/LoginController.java
@@ -4,11 +4,20 @@ import com.example.biz.user.UserVO;
 import com.example.biz.user.impl.UserDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-    @RequestMapping("/login.do")
+    @RequestMapping(value = "/login.do", method = RequestMethod.GET)
+    public String loginView(UserVO vo) {
+        vo.setId("test");
+        vo.setPassword("test1234");
+        return "login.jsp";
+    }
+
+    @RequestMapping(value = "/login.do", method = RequestMethod.POST)
     public String login(UserVO vo, UserDAO userDAO) {
+        System.out.println("로그인 인증 처리...");
         if (userDAO.getUser(vo) != null)
             return "getBoardList.do";
         else
---
+++ jsp/Spring/boardWeb/src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2023-07-17
+  Time: PM 2:15
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<center>
+    <h1>게시판 프로그램</h1>
+    <hr>
+    <a href="login.do">로그인</a><br>
+    <br>
+    <br> <a href="getBoardList.do">글 목록 바로가기</a>
+    <hr>
+</center>
+</body>
+</html>
