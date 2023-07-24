3373a0d1cea71edc8beea08593a0e4bd352e8eb6
dldlfdl2 <s21plus.choi@gmail.com>
2023-07-24 13:59:16 +0900

    07-24

--- jsp/Spring/boardWeb/src/test/java/com/example/biz/board/BoardServiceClient.java
+++ jsp/Spring/boardWeb/src/test/java/com/example/biz/board/BoardServiceClient.java
@@ -2,6 +2,8 @@ package com.example.biz.board;
 
 import com.example.biz.board.impl.BoardDAO;
 
+import java.util.List;
+
 public class BoardServiceClient {
     public static void main(String[] args) {
         BoardDAO boardDAO = new BoardDAO();
@@ -11,6 +13,16 @@ public class BoardServiceClient {
         vo.setWriter("홍길동");
         vo.setContent("myBatis내용");
 
-        boardDAO.insertBoard(vo);
+//        boardDAO.insertBoard(vo);
+
+        vo.setSearchCondition("TITLE");
+        vo.setSearchKeyword("");
+
+        List<BoardVO> boardVOList = boardDAO.getBoardList(vo);
+
+        for (BoardVO boardVO : boardVOList) {
+            System.out.println(">>>>" + boardVO.toString());
+        }
+
     }
 }
